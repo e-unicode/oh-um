3697dfc0c7668d47338f8c7f232f2b26103dd597
e-unicode <yangzzanga@gmail.com>
2023-02-16 20:03:11 +0900

    create login server

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.get('/', function (req, res) {
     응답.sendFile(path.join(__dirname, '/oh-um-front/build/index.html'));
   });
 
+//로그인 기능 만들기
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret: '비밀코드', resave: true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+
+
+
+
+
+
+
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "oh-um-front/build/index.html"));
+  res.sendFile(path.join(__dirname, "/oh-um-front/build/index.html"));
 });
